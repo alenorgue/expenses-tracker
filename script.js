418f93a0f5c3559e8a14bf68dce20f6881a0cb30
alenorgue <alenoriegag@gmail.com>
2025-04-30 20:24:20 +0200

    finished iteration 4

--- script.js
+++ script.js
@@ -2,15 +2,63 @@
 
 // Primeros pasos: carga el fichero JSON y muestra su contenido en la consola. Luego, ya puedes implementar la iteración 1. Para mostrar la fecha legible, puedes buscar por Chat GPT o por Google como convertir un timestamp 
 
+// Leer y parsear el archivo JSON
 const fs = require('fs');
+const data = fs.readFileSync('expenses.json', 'utf-8');
+const expenses = JSON.parse(data);
 
 if (process.argv.includes('--list')) {
-    // Leer y parsear el archivo JSON
-    const data = fs.readFileSync('expenses.json', 'utf-8');
-    const expenses = JSON.parse(data);
+  for (const expense of expenses) {
+    const fecha = new Date(expense.timestamp * 1000).toLocaleDateString();
+    console.log(`${expense.id} ${fecha} - ${expense.concept} - ${expense.category} - ${expense.amount}€`);
+  };
+}
 
-    expenses.forEach(expenses => {
-        const fecha = new Date(expenses.timestamp * 1000).toLocaleDateString();
-        console.log(`${expenses.id} ${fecha} - ${expenses.concept} [${expenses.category}] - ${expenses.amount}€ `);
-      });
+if (process.argv.includes('--summary')) {
+  let totalExpenses = 0;
+  for (const expense of expenses) {
+    totalExpenses += expense.amount;
+  }
+  console.log(`Total expenses: ${totalExpenses}€`);
+}
+
+
+let selectedCategory = null;
+
+
+for (const arg of process.argv) {
+  if (arg.startsWith('--filter-category=')) {
+    selectedCategory = arg.split('=')[1];
+    break;
+  }
+}
+
+if (selectedCategory) {
+  for (const expense of expenses) {
+    if (expense.category === selectedCategory) {
+      const fecha = new Date(expense.timestamp * 1000).toLocaleDateString();
+      console.log(`${expense.id} ${fecha} - ${expense.concept} - ${expense.category} - ${expense.amount}€`);
     }
+  }
+}
+
+
+const findIndex = process.argv.indexOf('--find');
+
+if (findIndex !== -1 && process.argv[findIndex + 1]) {
+  const idToFind = Number(process.argv[findIndex + 1]);
+  let found = false;
+
+  for (const expense of expenses) {
+    if (expense.id === idToFind) {
+      const fecha = new Date(expense.timestamp * 1000).toLocaleDateString();
+      console.log(`${expense.id} ${fecha} - ${expense.concept} [${expense.category}] - ${expense.amount}€`);
+      found = true;
+      break; // Terminamos el bucle porque ya lo encontramos
+    }
+  }
+
+  if (!found) {
+    console.log(`No expenses found with id = ${idToFind}`);
+  }
+}
